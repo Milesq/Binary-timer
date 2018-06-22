f327a86e77e6039dfa76c6f8848d53e542610531
Milesq <wmilosz88@gmail.com>
2018-06-22 20:39:21 +0200

    Final

--- main.py
+++ main.py
@@ -2,15 +2,21 @@ from display import getDisplayText as display
 from time import sleep
 from getTime import getTime
 import tkinter as tk
+import atexit
 
 app = tk.Tk()
 app.title("Binary Timer")
 
+x = tk.StringVar()
+
+label = tk.Label(app, textvariable=x)
+label.pack( side = tk.BOTTOM )
+
+atexit.register(lambda: label.destroy())
+
 while True:
-    # app.sele
-    label = tk.Label(app, text = '')
-    label.pack( side = tk.BOTTOM )
+    x.set(display(getTime()))
 
     app.update_idletasks()
     app.update()
-    sleep(1)
+    sleep(.005)
